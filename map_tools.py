c2359545681083c2e76fccbe66a7e48088f0ae89
Dmitry Torba <dmitrytorba@gmail.com>
2024-11-02 15:17:31 -0700

    feat(map) move map label

--- map_tools.py
+++ map_tools.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 
 @tool
-def move_map(lat: Annotated[str, "latitude"], lng: Annotated[str, "logitude"]) -> any:
-    """Move the users map center to a given latitude and longitude"""
+def move_map(lat: Annotated[str, "latitude"], lng: Annotated[str, "logitude"], label: Annotated[str, "map pin label"]) -> any:
+    """Move the users map center to a given latitude and longitude, placing a pin with a label"""
     return "Map moved to {},{}".format(lat, lng)
 
 
